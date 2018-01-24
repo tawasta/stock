fb186c0d60f7d52b310d0a304ed55f907175bb3f
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-01-24 10:31:58 +0200

    Add a new module for overriding inventory adjustment costs

---
+++ stock_inventory_adjustment_manual_cost/models/stock_inventory_line.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class StockInventoryLine(models.Model):
+
+    _inherit = 'stock.inventory.line'
+
+    price_unit = fields.Float(
+        string='Unit Price',
+        help='Technical field used to set the product quant cost',
+        # The digits-attribute is not provided,
+        # as the stock.move price_unit won't have it either
+        default=lambda self: self._get_default_price_unit(),
+    )
+
+    def _get_default_price_unit(self):
+        price_unit = 0
+        if self.product_id:
+            price_unit = self.product_id.standard_price
+
+        return price_unit
+
+    @api.onchange('product_id')
+    @api.depends('product_id')
+    def onchange_product_id_update_cost(self):
+        for record in self:
+            if record.product_id:
+                record.price_unit = record.product_id.standard_price
+
+    def _generate_moves(self):
+        return super(StockInventoryLine, self)._generate_moves()
+
+    def _get_move_values(self, qty, location_id, location_dest_id):
+        # Add manual cost price to lines
+
+        res = super(StockInventoryLine, self)._get_move_values(
+            qty=qty,
+            location_id=location_id,
+            location_dest_id=location_dest_id
+        )
+
+        res['price_unit'] = self.price_unit
+
+        return res
