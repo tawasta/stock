fbec1916b2fe1de941fc3ce2a189f0a6bd1a7072
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-04-22 23:44:23 +0300

    New module stock_pack_operation_running_number

---
+++ stock_pack_operation_running_number/README.rst
@@ -0,0 +1,37 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+================================
+Running Number For Picking lines
+================================
+
+* Adds a simple running number to picking lines
+
+Configuration
+=============
+\-
+
+Usage
+=====
+\-
+
+Known issues / Roadmap
+======================
+\-
+
+Credits
+=======
+
+Contributors
+------------
+* Timo Talvitie <timo.talvitie@tawasta.fi>
+
+Maintainer
+----------
+
+.. image:: http://tawasta.fi/templates/tawastrap/images/logo.png
+   :alt: Oy Tawasta OS Technologies Ltd.
+   :target: http://tawasta.fi/
+
+This module is maintained by Oy Tawasta OS Technologies Ltd.
---
+++ stock_pack_operation_running_number/models/stock_pack_operation.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class StockPackOperation(models.Model):
+
+    _inherit = 'stock.pack.operation'
+
+    @api.multi
+    def _compute_running_number(self):
+        '''Assigns a running number to the pack operation line,
+        starting from 1 onwards '''
+        for line in self:
+            pack_lines = self.search(args=[('picking_id',
+                                            '=',
+                                            line.picking_id.id)])
+            i = 1
+            for pack_line in pack_lines:
+                if pack_line.id == line.id:
+                    line.running_number = i
+                    break
+                i += 1
+
+    running_number = fields.Integer(
+        compute=_compute_running_number,
+        string='Running Number'
+    )
