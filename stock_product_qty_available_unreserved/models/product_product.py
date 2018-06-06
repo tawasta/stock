c7d7582847bb3a4649cf3d8e60a58655b7094735
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-06 10:27:03 +0300

    Add a new module to get the unreserved amount of products on hand

---
+++ stock_product_qty_available_unreserved/models/product_product.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+from odoo.addons import decimal_precision as dp
+
+
+class ProductProduct(models.Model):
+
+    _inherit = 'product.product'
+
+    qty_available_unreserved = fields.Float(
+        string='Unreserved quantity on hand',
+        compute='_compute_quantities',
+        search='_search_qty_available_unreserved',
+        digits=dp.get_precision('Product Unit of Measure'))
+
+    def _compute_quantities(self):
+        res = super(ProductProduct, self)._compute_quantities()
+
+        for product in self:
+            unreserved = product.qty_available - product.outgoing_qty
+            product.qty_available_unreserved = unreserved
+
