8f907641fa8bb900e8bca96c4e99dcf967688989
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-06-06 11:58:44 +0300

    Add module for showing warnings when using certain routes

---
+++ stock_location_route_warning/models/__init__.py
@@ -0,0 +1,2 @@
+from . import stock_location_route
+from . import sale_order_line
---
+++ stock_location_route_warning/models/sale_order_line.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    @api.multi
+    @api.onchange('product_id')
+    def onchange_product_id_route_warning(self):
+
+        product = self.product_id
+
+        if not product:
+            return
+
+        for route in product.route_ids:
+            if route.sale_line_warn != 'no-message':
+                title = _("Warning for %s") % product.name
+                message = route.sale_line_warn_msg
+
+                warning = dict(
+                    title=title,
+                    message=message,
+                )
+
+            result = {'warning': warning}
+
+            if route.sale_line_warn == 'block':
+                self.product_id = False
+                return result
+
+        return result
