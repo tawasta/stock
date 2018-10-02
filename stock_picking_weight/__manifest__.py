5ce6a733353abfbf67d9acccc1350a49d1879fab
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-10-02 12:00:39 +0300

    Update i18n

--- stock_picking_weight/__manifest__.py
+++ stock_picking_weight/__manifest__.py
@@ -23,7 +23,7 @@
     'name': 'Weight on stock picking and pack operation lines',
     'summary': 'Add weight on stock picking and operations',
     'category': 'Warehouse',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
