05d6b8553bf367ddc843f06af1ef62c521e10e2e
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-10-02 14:08:42 +0300

    Add the return logic

--- stock_picking_return_create_sale/wizard/stock_return_picking.py
+++ stock_picking_return_create_sale/wizard/stock_return_picking.py
@@ -10,14 +10,30 @@ class StockReturnPicking(models.TransientModel):
     def create_returns(self):
         res = super(StockReturnPicking, self).create_returns()
 
-        res_id = res.get('res_id')
-        if res_id:
-            picking_id = self.env['stock.picking'].browse([res_id])
+        picking_id = self.env['stock.picking'].browse(
+            self.env.context.get('active_id'))
 
         if picking_id and picking_id.sale_id:
             # If the sale is already invoiced, create a new sale order
             if picking_id.sale_id.invoice_status == 'invoiced':
-                # Create a new sale order
-                pass
+                refund_moves = self.product_return_moves.filtered(
+                    'to_refund_so',
+                )
+
+                SaleOrder = self.env['sale.order']
+                SaleOrderLine = self.env['sale.order.line']
+
+                if refund_moves:
+                    # Create a new sale order
+                    sale_order = SaleOrder.create(dict(
+                        partner_id=picking_id.sale_id.partner_id.id,
+                    ))
+
+                for move in refund_moves:
+                    SaleOrderLine.create(dict(
+                        order_id=sale_order.id,
+                        product_id=move.product_id.id,
+                        product_uom_qty=-move.quantity,
+                    ))
 
         return res
