57691a08748e1b17b04dc089c0cdb3d7eec534de
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-10-30 10:30:20 +0200

    Add a new module for disabling automated delivery charges

---
+++ stock_disable_invoice_shipping_on_delivery/models/sale_order.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_confirm(self):
+        # Override the 'invoice_shipping_on_delivery' to always be False
+        res = super(SaleOrder, self).action_confirm()
+        for so in self:
+            # invoice_shipping_on_delivery is always False
+            so.invoice_shipping_on_delivery = False
+        return res
