65d0b816a00f6c075ebd51b60c7ab8f8245d9915
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-04-11 14:41:54 +0300

    [IMP] Location auto-generation logic

--- stock_location_partner/models/sale_order.py
+++ stock_location_partner/models/sale_order.py
@@ -8,33 +8,7 @@ class SaleOrder(models.Model):
 
     @api.multi
     def action_confirm(self):
-        # If partner doesn't have own location, create one
-        StockLocation = self.env['stock.location']
-
-        # TODO: allow choosing a default location, if partner has many
-        customer_location = StockLocation.search([
-            ('partner_id', '=', self.partner_shipping_id.id),
-            ('company_id', '=', self.company_id.id),
-            ('usage', '=', 'customer'),
-        ], limit=1)
-
-        if not customer_location:
-            default_location = self.env.ref('stock.stock_location_customers')
-
-            location_values = {
-                'name': self.partner_shipping_id.name,
-                'usage': 'customer',
-                'location_id': default_location.id,
-                'company_id': self.company_id.id,
-                'partner_id': self.partner_shipping_id.id,
-            }
-
-            customer_location = StockLocation.sudo().create(location_values)
-
-            # Don't overwrite a custom location
-            if self.partner_shipping_id.property_stock_customer \
-                    == default_location:
-                self.partner_shipping_id.property_stock_customer \
-                    = customer_location
+        for record in self:
+            record.partner_shipping_id.create_stock_location()
 
         return super(SaleOrder, self).action_confirm()
