e296798260bdf4431daa74382cac60253d9658e4
difbit <36134879+difbit@users.noreply.github.com>
2020-04-06 08:48:07 +0300

    12.0 dev stock bypass reservation (#49)
    * [ADD] Readme file
    
    * [ADD] init-file: imports models
    
    * [ADD] The module descriptor file
    
    * [ADD] Translation file
    
    * [ADD] init-file: imports stock_location
    
    * [ADD] Python file: adds a field
    
    * [ADD] Module's icon
    
    * [ADD] Module's view file

---
+++ stock_picking_bypass_reservation/models/stock_location.py
@@ -0,0 +1,19 @@
+from odoo import fields, models
+
+
+class StockLocation(models.Model):
+
+    _inherit = "stock.location"
+
+    allow_bypass_reservation = fields.Boolean(
+        string="Always allow stock moves",
+        help="""When selected, always allow stock moves on this location for every
+        product. Negative stock quants are permitted because of this."""
+    )
+
+    def should_bypass_reservation(self):
+        res = super(StockLocation, self).should_bypass_reservation()
+        if self.allow_bypass_reservation:
+            return True
+        else:
+            return res
