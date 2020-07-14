4156d3ee4e73cf72f87089b17c019bfbbef29980
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2020-07-14 09:51:04 +0300

    [ADD] New module sale_stock_disable_warehouse_onchange
    Disable company change on warehouse change

--- stock_deliver_from_sale_order/models/sale_order.py
+++ stock_deliver_from_sale_order/models/sale_order.py
@@ -3,7 +3,7 @@ from odoo import models, api, _, exceptions
 
 class SaleOrder(models.Model):
 
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     @api.multi
     def deliver(self):
@@ -13,12 +13,15 @@ class SaleOrder(models.Model):
         self.ensure_one()
         self.action_confirm()
 
-        incomplete_pickings = \
-            [x for x in self.picking_ids if x.state not in ['cancel', 'done']]
+        incomplete_pickings = [
+            x for x in self.picking_ids if x.state not in ["cancel", "done"]
+        ]
 
         if len(incomplete_pickings) == 0:
-            msg = _('{} does not have a pending delivery order, please '
-                    'deliver the sale order manually.'.format(self.name))
+            msg = _(
+                "{} does not have a pending delivery order, please "
+                "deliver the sale order manually.".format(self.name)
+            )
             raise exceptions.ValidationError(msg)
 
         elif len(incomplete_pickings) > 1:
@@ -26,9 +29,11 @@ class SaleOrder(models.Model):
             # raise an exception.
             # This is because the autodelivery function utilizes
             # the delivery wizard, which expects only a single delivery order.
-            msg = _('You can autodeliver a sale order only if it has a single '
-                    'delivery order waiting, but {} has multiple. Please '
-                    'deliver the sale order manually'.format(self.name))
+            msg = _(
+                "You can autodeliver a sale order only if it has a single "
+                "delivery order waiting, but {} has multiple. Please "
+                "deliver the sale order manually".format(self.name)
+            )
             raise exceptions.ValidationError(msg)
 
         else:
@@ -39,15 +44,16 @@ class SaleOrder(models.Model):
             # process method manually
             res = incomplete_pickings[0].do_new_transfer()
 
-            if res['res_model'] != 'stock.immediate.transfer':
+            if res["res_model"] != "stock.immediate.transfer":
                 # In case Odoo for some reason returned
                 # stock.backorder.confirmation wizard in
                 # do_new_transfer() instead
                 raise exceptions.except_orm(
-                    _('Error'),
-                    _('Immediate transfer not possible, '
-                      'please deliver the sale order manually'))
+                    _("Error"),
+                    _(
+                        "Immediate transfer not possible, "
+                        "please deliver the sale order manually"
+                    ),
+                )
             else:
-                self.env['stock.immediate.transfer'].browse(
-                    res['res_id']
-                ).process()
+                self.env["stock.immediate.transfer"].browse(res["res_id"]).process()
