877c5b0a5166e495c30f0cc0f378356b9c18c562
difbit <timo.kekalainen@hotmail.com>
2020-08-10 13:28:48 +0300

    [ADD] Adds store attribute for inventory valuation fields

---
+++ stock_inventory_valuation_sort_tree/models/product_product.py
@@ -0,0 +1,11 @@
+
+from odoo import fields, models
+
+
+class ProductProduct(models.Model):
+
+    _inherit = 'product.product'
+
+    stock_value = fields.Float(store=True)
+    qty_at_date = fields.Float(store=True)
+    display_name = fields.Char(store=True)
