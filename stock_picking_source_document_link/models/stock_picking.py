ddd75877a89c2053ecb0bcf85d377d877277081a
difbit <36134879+difbit@users.noreply.github.com>
2020-08-12 16:22:56 +0300

    [FIX] For-loop added to avoid singleton error (#183)
    * [FIX] For-loop added to avoid singleton error
    
    * [FIX] False needs to written with capital letter

--- stock_picking_source_document_link/models/stock_picking.py
+++ stock_picking_source_document_link/models/stock_picking.py
@@ -25,30 +25,31 @@ class StockPicking(models.Model):
 
     @api.depends("origin")
     def _compute_source_document_link(self):
-        origin = str(self.origin)
+        for pick in self:
+            origin = str(pick.origin)
 
-        def get_model_by_name_or_false(model, name):
-            links = self.env[model].search([('name', '=', name)])
-            if len(links) >= 1:
-                return links[0]
-            else:
-                return False
+            def get_model_by_name_or_false(model, name):
+                links = self.env[model].search([('name', '=', name)])
+                if len(links) >= 1:
+                    return links[0]
+                else:
+                    return False
 
-        if origin.startswith("SO"):
-            self.source_document_link_sale_order = \
-                get_model_by_name_or_false(
-                    'sale.order',
-                    origin
-                )
-        elif origin.startswith("PO"):
-            self.source_document_link_purchase_order = \
-                get_model_by_name_or_false(
-                    'purchase.order',
-                    origin
-                )
-        elif origin.startswith("Return of WH"):
-            self.source_document_link_stock_picking = \
-                get_model_by_name_or_false(
-                    'stock.picking',
-                    origin.replace("Return of ", ""),
-                )
+            if origin.startswith("so"):
+                pick.source_document_link_sale_order = \
+                    get_model_by_name_or_false(
+                        'sale.order',
+                        origin
+                    )
+            elif origin.startswith("po"):
+                pick.source_document_link_purchase_order = \
+                    get_model_by_name_or_false(
+                        'purchase.order',
+                        origin
+                    )
+            elif origin.startswith("return of wh"):
+                pick.source_document_link_stock_picking = \
+                    get_model_by_name_or_false(
+                        'stock.picking',
+                        origin.replace("return of ", ""),
+                    )
