f8316d04ce65e765a34529887effb2bd16a2379c
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2022-02-18 12:52:28 +0000

    12.0 dev stock picking process as sudo

---
+++ stock_picking_process_as_sudo/wizards/stock_backorder_confirmation.py
@@ -0,0 +1,12 @@
+
+from odoo import models
+
+
+class StockBackorderConfirmation(models.TransientModel):
+    _inherit = 'stock.backorder.confirmation'
+
+    def sudo_process(self):
+        self.sudo()._process()
+
+    def sudo_process_cancel_backorder(self):
+        self.sudo().process_cancel_backorder()
