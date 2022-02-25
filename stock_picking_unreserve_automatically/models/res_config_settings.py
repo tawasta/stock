cfc461a47dcea650cbac3fe9e1f90cb5c0285cdc
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2022-02-25 15:26:11 +0000

    [ADD] The main functionality: automatically unreserve deliveries if

---
+++ stock_picking_unreserve_automatically/models/res_config_settings.py
@@ -0,0 +1,13 @@
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = "res.config.settings"
+
+    force_unreserve = fields.Boolean(
+        string="Force Unreserve",
+        related="company_id.force_unreserve",
+        readonly=False,
+    )
---
+++ stock_picking_unreserve_automatically/models/sale_order.py
@@ -0,0 +1,13 @@
+
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    def _default_force_unreserve(self):
+        return self.env.user.company_id.force_unreserve
+
+    force_unreserve = fields.Boolean(
+        default=lambda self: self._default_force_unreserve())
