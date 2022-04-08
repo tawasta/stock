550777cd7f79b17751d8e387cf56e205434820ba
difbit <timo.kekalainen@hotmail.com>
2022-04-08 12:56:59 +0300

    [IMP] action_cancel modification is done on stock.picking

--- stock_picking_unreserve_automatically/models/sale_order.py
+++ stock_picking_unreserve_automatically/models/sale_order.py
@@ -11,3 +11,14 @@ class SaleOrder(models.Model):
 
     force_unreserve = fields.Boolean(copy=False,
         default=lambda self: self._default_force_unreserve())
+
+    @api.multi
+    def action_cancel(self):
+        """ In case a sale order is canceled and confirmed again, it is
+            necessary to set a procurement group's force_unreserve-field
+            to True. This way the sale order's new transfer will not be
+            reserved. """
+        for order in self:
+            if order.group_id:
+                order.group_id.force_unreserve = True
+            return super(SaleOrder, order).action_cancel()
