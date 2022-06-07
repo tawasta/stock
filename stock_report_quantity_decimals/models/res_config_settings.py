c9bb6e1e7b8db05ae54915010e725effe6c53cd5
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2022-06-07 11:56:01 +0000

    [MIG] Migration to 14.0 version. Made some changes because Odoo's report works differently

---
+++ stock_report_quantity_decimals/models/res_config_settings.py
@@ -0,0 +1,18 @@
+from odoo import api, fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+
+    _inherit = 'res.config.settings'
+
+    stock_report_decimal_precision = fields.Integer(
+        related='company_id.stock_report_decimal_precision',
+        readonly=False,
+    )
+
+    @api.onchange('stock_report_decimal_precision')
+    def onchange_stock_report_decimal_precision(self):
+        company_precision = self.env.ref(
+            'stock_report_quantity_decimals.decimal_company_precision')
+
+        company_precision.digits = self.stock_report_decimal_precision
