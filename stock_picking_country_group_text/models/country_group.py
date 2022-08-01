12eb669220fb4c8e1cf5fc4b65f7b9204218475e
difbit <timo.kekalainen@hotmail.com>
2022-08-01 15:05:26 +0300

    [MIG] Migration of stock_picking_country_group_text module to 14.0 version

---
+++ stock_picking_country_group_text/models/country_group.py
@@ -0,0 +1,20 @@
+from odoo import fields, models
+
+
+class CountryGroup(models.Model):
+
+    _inherit = "res.country.group"
+
+    stock_picking_text = fields.Text(
+        string="Stock Picking Text",
+        help="Text to be added on stock pickings going to customers in this "
+        + "country group.",
+        translate=True,
+    )
+
+    delivery_slip_text = fields.Text(
+        string="Delivery Slip Text",
+        help="Text to be added on delivery slip going to customers in this "
+        + "country group.",
+        translate=True,
+    )
