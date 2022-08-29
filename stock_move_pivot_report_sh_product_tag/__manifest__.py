cc65df7d129367a916098c54d0f9310cc561041f
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2022-08-29 19:11:36 +0000

    12.0 dev stock move pivot report sh product tag

---
+++ stock_move_pivot_report_sh_product_tag/__manifest__.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2022 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Reporting of Stock Moves - Group by SH product tags",
+    "summary": "Group by SH product tags in Stock Move pivot view",
+    "version": "12.0.1.0.0",
+    "category": "Reporting",
+    "website": "https://gitlab.com/tawasta/odoo/stock",
+    "author": "Tawasta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["sh_product_variant_tags", "stock", "queue_job"],
+    "data": ["data/ir_cron.xml", "views/stock_move.xml"],
+}
