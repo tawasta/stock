9682054a614a571b9d20ee97d5738d0383551cf4
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-01-26 06:44:34 +0000

    [FIX] Show report name if that option has been selected

--- stock_report_title/__manifest__.py
+++ stock_report_title/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Stock Picking and Delivery Slip Report Title",
     "summary": "Stock Picking and Delivery Slip Report Title",
-    "version": "14.0.2.0.0",
+    "version": "14.0.2.0.1",
     "category": "Reporting",
     "website": "https://gitlab.com/tawasta/odoo/stock",
     "author": "Tawasta",
