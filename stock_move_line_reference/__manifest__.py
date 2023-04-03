08f68605ac6534d38f9de9d702a85c0b38f28ba9
difbit <timo.kekalainen@tawasta.fi>
2023-04-03 13:31:33 +0300

    [IMP] stock_move_line_reference module: optional origin-field

--- stock_move_line_reference/__manifest__.py
+++ stock_move_line_reference/__manifest__.py
@@ -22,7 +22,7 @@
     "name": "Stock move line reference",
     "summary": "Renames Stock Picking field as Transfer Reference",
     "category": "Stock",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "website": "https://gitlab.com/tawasta/odoo/stock",
     "author": "Tawasta",
     "license": "AGPL-3",
