09009ec3764ef75f83eaf02a8fb64013fd41e521
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-05-07 07:28:19 +0000

    [FIX] stock_report_picking_stock_available_quantity module: fixed "stock.quant()" shown when

--- stock_report_picking_stock_available_quantity/__manifest__.py
+++ stock_report_picking_stock_available_quantity/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Picking list - Available Quantity",
     "summary": "Adds Available Quantity to picking list",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Stock",
     "website": "https://gitlab.com/tawasta/odoo/stock",
     "author": "Tawasta",
