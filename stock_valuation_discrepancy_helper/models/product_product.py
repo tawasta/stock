986236c43e4f23fb4daa1afba57de17304b56c34
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2023-07-13 13:22:53 +0300

    [ADD] Re-add valuation fix actions

--- stock_valuation_discrepancy_helper/models/product_product.py
+++ stock_valuation_discrepancy_helper/models/product_product.py
@@ -1,23 +1,23 @@
-from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
+from odoo import models
 
 
 class Product(models.Model):
     _inherit = "product.product"
 
     def action_sync_with_on_hand(self):
-        raise ValidationError(_("Disabled"))
         svl = self.env["stock.valuation.layer"]
         for record in self:
-            layers = svl.search([("product_id", "=", record.id)], order="create_date DESC")
+            layers = svl.search(
+                [("product_id", "=", record.id)], order="create_date DESC"
+            )
             layer0 = layers[0]
 
             qty = sum(layers.mapped("quantity"))
             remaining_qty = sum(layers.mapped("remaining_qty"))
 
             if qty != record.qty_available:
-                new_qty = -(qty-record.qty_available)
-                new_remaining_qty = -(remaining_qty-record.qty_available)
+                new_qty = -(qty - record.qty_available)
+                new_remaining_qty = -(remaining_qty - record.qty_available)
                 unit_cost = record.standard_price
                 values = {
                     "product_id": record.id,
