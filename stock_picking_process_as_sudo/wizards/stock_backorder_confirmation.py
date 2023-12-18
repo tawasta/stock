daed855a16ddf89f0e777f9fb97ad333944a6627
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-12-18 09:56:30 +0000

    [IMP] stock_picking_process_as_sudo module: close wizard window after

--- stock_picking_process_as_sudo/wizards/stock_backorder_confirmation.py
+++ stock_picking_process_as_sudo/wizards/stock_backorder_confirmation.py
@@ -2,10 +2,21 @@ from odoo import models
 
 
 class StockBackorderConfirmation(models.TransientModel):
+
     _inherit = "stock.backorder.confirmation"
 
     def sudo_process(self):
-        return self.sudo().process()
+        close_window = {"type": "ir.actions.act_window_close"}
+
+        return {
+            "type": "ir.actions.act_multi",
+            "actions": [self.sudo().process(), close_window],
+        }
 
     def sudo_process_cancel_backorder(self):
-        return self.sudo().process_cancel_backorder()
+        close_window = {"type": "ir.actions.act_window_close"}
+
+        return {
+            "type": "ir.actions.act_multi",
+            "actions": [self.sudo().process_cancel_backorder(), close_window],
+        }
