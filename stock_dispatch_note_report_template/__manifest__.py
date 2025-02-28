ded561a1cb9650f525f08da03fa94cf03cd33a71
difbit <timo.kekalainen@tawasta.fi>
2025-02-28 10:04:27 +0200

    [FIX] stock_dispatch_note_report_template module: Small fix to template

--- stock_dispatch_note_report_template/__manifest__.py
+++ stock_dispatch_note_report_template/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Stock Dispatch Note Template",
     "summary": "Adds weights, dates and clickable print is shown as 'Dispatch Note'",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "category": "Reporting",
     "website": "https://gitlab.com/tawasta/odoo/stock",
     "author": "Tawasta",
