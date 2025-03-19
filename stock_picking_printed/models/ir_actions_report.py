f749c21bafe952bd7c46077a3c46bbd7cee00410
Timo Talvitie <timo.talvitie@tawasta.fi>
2025-03-19 12:50:20 +0200

    [MIG] stock_picking_printed from 14.0 to 17.0

---
+++ stock_picking_printed/models/ir_actions_report.py
@@ -0,0 +1,22 @@
+from odoo import fields, models
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class IrActionsReport(models.Model):
+    _inherit = "ir.actions.report"
+
+    def _is_stock_picking_report(self, report_ref):
+        """Check if the stock picking report is being printed"""
+        return self._get_report(report_ref).report_name == "stock.report_picking"
+
+    def _render_qweb_pdf(self, report_ref, res_ids=None, data=None):
+        """When printing picking, log the printing date"""
+        if self._is_stock_picking_report(report_ref):
+            _logger.info("is picking report ")
+            pickings = self.env["stock.picking"].browse(res_ids)
+            pickings.write({"picking_printed": fields.Datetime.now()})
+
+        return super()._render_qweb_pdf(report_ref, res_ids=res_ids, data=data)
