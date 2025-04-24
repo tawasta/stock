da4ac657e24a2b9f3f39317596fe33a01c7e1dfd
difbit <timo.kekalainen@tawasta.fi>
2025-04-24 14:15:29 +0300

    [FIX] stock_picking_comment module: Added a small condition to avoid False values

--- stock_picking_comment/models/sale_order.py
+++ stock_picking_comment/models/sale_order.py
@@ -18,41 +18,17 @@
 #
 ##############################################################################
 
-# 1. Standard library imports:
-
-# 2. Known third party imports:
-
-# 3. Odoo imports (openerp):
 from odoo import models
 from odoo.tools import html2plaintext
 
-# 4. Imports from Odoo modules:
-
-# 5. Local imports in the relative form:
-
-# 6. Unknown third party imports:
-
 
 class SaleOrder(models.Model):
-    # 1. Private attributes
     _inherit = "sale.order"
 
-    # 2. Fields declaration
-
-    # 3. Default methods
-
-    # 4. Compute and search fields, in the same order that fields declaration
-
-    # 5. Constraints and onchanges
-
-    # 6. CRUD methods
-
-    # 7. Action methods
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
         for pick in self.picking_ids:
-            note = html2plaintext(self.note) or ""
-            pick.write({"comment": note})
+            if self.note:
+                note = html2plaintext(self.note) or ""
+                pick.write({"comment": note})
         return res
-
-    # 8. Business methods
