5c6b582f387038665d8847c432572925e4b2ee35
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2025-05-06 13:58:10 +0300

    [REF] Correct author

--- stock_report_scheduled_date_as_date_only/__manifest__.py
+++ stock_report_scheduled_date_as_date_only/__manifest__.py
@@ -1,7 +1,7 @@
 ##############################################################################
 #
-#    Author: Oy Tawasta OS Technologies Ltd.
-#    Copyright 2020 Oy Tawasta OS Technologies Ltd. (https://tawasta.fi)
+#    Author: Futural Oy
+#    Copyright 2020 Futural Oy (https://futural.fi)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -24,7 +24,7 @@
     "version": "17.0.1.0",
     "category": "Reporting",
     "website": "https://gitlab.com/tawasta/odoo/stock",
-    "author": "Tawasta",
+    "author": "Futural",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
