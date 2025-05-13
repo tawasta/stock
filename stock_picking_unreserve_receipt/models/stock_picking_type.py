b2fe7683fedeac5d14eebc763df2a0eb22c0cd38
difbit <timo.kekalainen@tawasta.fi>
2025-05-13 13:39:08 +0300

    [ADD] A new module: stock_picking_unreserve_receipt. It is used to automatically unreserve receipts

---
+++ stock_picking_unreserve_receipt/models/stock_picking_type.py
@@ -0,0 +1,18 @@
+from odoo import api, fields, models
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    unreserve_receipt = fields.Boolean(
+        string="Unreserve receipt automatically",
+        copy=False,
+        default=False,
+        help="Pickings created by this picking type will be unreserved automatically upon creating them.",
+    )
+    hide_unreserve_receipt = fields.Boolean(compute="_compute_hide_unreserve_receipt")
+
+    @api.depends("code")
+    def _compute_hide_unreserve_receipt(self):
+        for rec in self:
+            rec.hide_unreserve_receipt = rec.code != "incoming"
