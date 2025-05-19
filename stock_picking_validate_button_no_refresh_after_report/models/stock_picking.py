a195ff73b07545c92f5f64e327921af9638a2193
difbit <timo.kekalainen@tawasta.fi>
2025-05-19 10:10:34 +0300

    [ADD] A new module: stock_picking_validate_button_no_refresh_after_report. Prevents reloading a page after validating pickings

---
+++ stock_picking_validate_button_no_refresh_after_report/models/stock_picking.py
@@ -0,0 +1,16 @@
+from odoo import models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def button_validate(self):
+        res = super().button_validate()
+
+        if res.get("params", False):
+            res["params"]["anotherAction"] = {
+                "type": "ir.actions.client",
+                "tag": "soft_reload",
+            }
+
+        return res
