3ebb50440a1254ab38d2200be0200ec8a66afb8d
difbit <timo.kekalainen@tawasta.fi>
2025-06-02 14:57:25 +0300

    [ADD] A new module: stock_warehouse_replenishment_alert_send_message. It adds a cron which checks if a product on an orderpoint has its quantity changed below its minimium quantity

---
+++ stock_warehouse_replenishment_alert_send_message/README.rst
@@ -0,0 +1,42 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+================================================================================
+Send a message if Qty On Hand drops below its minumum quantity in replenishments
+================================================================================
+
+Send a message for the responsible if Quantity drops below zero in replenishments.
+
+Configuration
+=============
+Check that products in replenishments have their respective responsible set
+so that sending messages is possible.
+
+Usage
+=====
+Go to Replenishments to see their Quantities on hand. If this quantity
+drops below zero, the responsible of a product receives an alert message
+about this.
+
+Known issues / Roadmap
+======================
+This module works with onchange and it is possible that this won't always
+trigger and therefore send a message.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Timo Kekäläinen <timo.kekalainen@tawasta.fi>
+
+Maintainer
+----------
+
+.. image:: http://tawasta.fi/templates/tawastrap/images/logo.png
+   :alt: Oy Tawasta OS Technologies Ltd.
+   :target: http://tawasta.fi/
+
+This module is maintained by Oy Tawasta OS Technologies Ltd.
---
+++ stock_warehouse_replenishment_alert_send_message/models/stock_warehouse_orderpoint.py
@@ -0,0 +1,67 @@
+from odoo import _, api, fields, models
+
+
+class StockWarehouseOrderpoint(models.Model):
+    _name = "stock.warehouse.orderpoint"
+    _inherit = ["stock.warehouse.orderpoint", "mail.thread"]
+
+    # Stored field which is used for alerts, not its computed counterpart
+    qty_on_hand_to_check = fields.Float(
+        store=True,
+        copy=False,
+        default=lambda self: self._default_qty_on_hand_to_check(),
+    )
+
+    def _default_qty_on_hand_to_check(self):
+        return self.qty_on_hand
+
+    def cron_qty_on_hand_alert(self):
+        """Check if quantity drops below minimium quantity. Send a message
+        if a responsible is found from product. Don't send a message if it
+        has been previously sent and the quantity has not changed after
+        earlier message."""
+
+        orderpoints = self.env["stock.warehouse.orderpoint"].search([])
+
+        for orderpoint in orderpoints:
+            responsible = (
+                orderpoint.product_id and orderpoint.product_id.responsible_id or False
+            )
+            responsible_email = responsible and responsible.partner_id.email or False
+            quantity_has_changed = (
+                orderpoint.qty_on_hand_to_check != orderpoint.qty_on_hand
+            )
+
+            if (
+                orderpoint.qty_on_hand < orderpoint.product_min_qty
+                and responsible_email
+                and quantity_has_changed
+            ):
+                display_msg = _(
+                    """<div style="color: red;">
+                    Replenishment for location: {}
+                    <br/>
+                    The quantity of {} product is below its minimium quantity ({}) and it is now {}
+                    </div>
+                    """.format(
+                        orderpoint.location_id and orderpoint.location_id.name or "",
+                        orderpoint.product_id.display_name,
+                        orderpoint.product_min_qty,
+                        orderpoint.qty_on_hand,
+                    )
+                )
+
+                orderpoint.message_post(
+                    message_type="notification",
+                    subject=_(
+                        """Alert because the quantity on hand of product {} has dropped
+                       below its minimum quantity""".format(
+                            orderpoint.product_id.display_name,
+                        )
+                    ),
+                    body=display_msg,
+                    partner_ids=[responsible.partner_id.id],
+                    body_is_html=True,
+                )
+
+            orderpoint.qty_on_hand_to_check = orderpoint.qty_on_hand
