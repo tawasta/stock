f91c758c926e0f62e82265056079960e5405a356
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-03-04 12:27:31 +0200

    Delete options to remove things that do not exists on report

--- stock_report_title/models/res_config_settings.py
+++ stock_report_title/models/res_config_settings.py
@@ -25,18 +25,8 @@ class ResConfigSettings(models.TransientModel):
         related="company_id.hide_delivery_slip_report_name",
         readonly=False,
     )
-    hide_picking_title = fields.Boolean(
-        string="Hide picking title",
-        related="company_id.hide_picking_title",
-        readonly=False,
-    )
     hide_picking_report_name = fields.Boolean(
         string="Hide picking report name",
         related="company_id.hide_picking_report_name",
-        readonly=False,
-    )
-    hide_picking_name_in_header = fields.Boolean(
-        string="Hide picking name in header",
-        related="company_id.hide_picking_name_in_header",
         readonly=False,
     )
