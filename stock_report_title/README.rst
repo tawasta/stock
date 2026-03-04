f8b2f9f381167b0374abff82b27a2e9cf4bb6731
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-03-04 11:58:32 +0200

    [MIG] 19.0 stock_report_title

---
+++ stock_report_title/README.rst
@@ -0,0 +1,43 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+============================================
+Stock Picking and Delivery Slip Report Title
+============================================
+
+* Hides title and adds it on top of the page and in informations elements instead
+* Can be configured in res.config.settings
+* Change Delivery Slip title from the inventory settings. Many installations
+  want different titles, so this is a useful feature.
+
+Configuration
+=============
+\-
+
+Usage
+=====
+Just install the module.
+
+Known issues / Roadmap
+======================
+\-
+
+Credits
+=======
+
+Contributors
+------------
+
+* Timo Kekäläinen <timo.kekalainen@futural.fi>
+* Miika Nissi <miika.nissi@futural.fi>
+* Joona Isoaho <joona.isoaho@futural.fi>
+
+Maintainer
+----------
+
+.. image:: https://futural.fi/templates/tawastrap/images/logo.png
+   :alt: Futural Oy
+   :target: https://futural.fi/
+
+This module is maintained by Futural Oy
---
+++ stock_report_title/models/res_config_settings.py
@@ -0,0 +1,42 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    delivery_slip_title = fields.Char(
+        string="Delivery slip title",
+        related="company_id.delivery_slip_title",
+        readonly=False,
+        translate=True,
+    )
+    hide_delivery_slip_title = fields.Boolean(
+        string="Hide delivery slip title",
+        related="company_id.hide_delivery_slip_title",
+        readonly=False,
+    )
+    hide_delivery_slip_text = fields.Boolean(
+        string="Hide delivery slip text",
+        related="company_id.hide_delivery_slip_text",
+        readonly=False,
+    )
+    hide_delivery_slip_report_name = fields.Boolean(
+        string="Hide delivery slip report name",
+        related="company_id.hide_delivery_slip_report_name",
+        readonly=False,
+    )
+    hide_picking_title = fields.Boolean(
+        string="Hide picking title",
+        related="company_id.hide_picking_title",
+        readonly=False,
+    )
+    hide_picking_report_name = fields.Boolean(
+        string="Hide picking report name",
+        related="company_id.hide_picking_report_name",
+        readonly=False,
+    )
+    hide_picking_name_in_header = fields.Boolean(
+        string="Hide picking name in header",
+        related="company_id.hide_picking_name_in_header",
+        readonly=False,
+    )
